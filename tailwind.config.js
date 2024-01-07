7b5e318b283f99463c741e2b367fee474448e7c4
sean-lgt <852951042@qq.com>
2024-01-07 11:10:45 +0800

    feat: tailwind 初体验

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // 应用到 index 文件 src目录下的文件
+  content: ['./index.html', './src/**/*.{vue,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
