58aba9db0a992973941f391022fa6e59bb016342
林桂涛 <852951042@qq.com>
2024-01-25 15:41:28 +0800

    perf: 使用 vueuse 工具函数库提供的 useWindowSize 响应式处理当前是否为移动端设备

--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -1,5 +1,8 @@
 import { computed } from 'vue'
+import { useWindowSize } from '@vueuse/core'
 import { PC_DEVICE_WIDTH } from '../constants'
+
+const { width } = useWindowSize()
 
 /**
  * @description: 判断当前是否为移动端设备，依据屏幕宽度是否小于一个指定宽度 （1280）
@@ -7,5 +10,5 @@ import { PC_DEVICE_WIDTH } from '../constants'
  * @return {Boolean}
  */
 export const isMobileTerminal = computed(() => {
-  return document.documentElement.clientWidth < PC_DEVICE_WIDTH
+  return width.value < PC_DEVICE_WIDTH
 })
