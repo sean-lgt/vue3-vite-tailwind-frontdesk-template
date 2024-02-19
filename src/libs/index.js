961bc3b5f01031403457eb2ed6cd10f6983f907d
sean-lgt <852951042@qq.com>
2024-02-19 22:10:27 +0800

    feat: 通用组件自动化注册

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,10 +1,15 @@
-import svgIcons from './svg-icon/index.vue'
-import popup from './popup/index.vue'
+import { defineAsyncComponent } from 'vue'
 
 export default {
   install(app) {
-    // 注册组件
-    app.component('m-svg-icon', svgIcons)
-    app.component('m-popup', popup)
+    // 获取当前路径任意文件夹下的 index.vue 文件
+    const components = import.meta.glob('./*/index.vue')
+    // 遍历获取到的组件模块
+    for (const [key, value] of Object.entries(components)) {
+      // 拼接组件注册的 name
+      const componentName = 'm-' + key.replace('./', '').split('/')[0]
+      // 异步注册组件
+      app.component(componentName, defineAsyncComponent(value))
+    }
   }
 }
