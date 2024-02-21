05cbb01182cd7e67ac2cacdce11f24ac7d049b70
sean-lgt <852951042@qq.com>
2024-02-21 22:01:56 +0800

    feat: 基于 popover 处理用户模块

--- src/views/layout/components/header/header-my/index.vue
+++ src/views/layout/components/header/header-my/index.vue
@@ -1,7 +1,65 @@
 <template>
-  <div class="w-10 text-xl">my</div>
+  <m-popover class="flex items-center" placement="bottom-left">
+    <template #reference>
+      <div
+        class="relative flex items-center p-0.5 rounded-sm cursor-pointer duration-200 outline-none hover:bg-zinc-100"
+      >
+        <img
+          class="w-3 h-3 rounded-sm"
+          src="https://file.ipadown.com/tophub/assets/images/media/mp.weixin.qq.com.png_160x160.png"
+        />
+        <m-svg-icon
+          class="h-1.5 w-1.5 ml-0.5"
+          name="down-arrow"
+          fillClass="fill-zinc-900"
+        ></m-svg-icon>
+        <m-svg-icon
+          class="h-1.5 w-1.5 absolute right-[16px] bottom-0"
+          name="vip"
+          fillClass="fill-zinc-900"
+        ></m-svg-icon>
+      </div>
+    </template>
+    <div class="w-[140px] overflow-hidden p-1">
+      <div
+        class="flex items-center p-1 cursor-pointer rounded hover:bg-zinc-100/60 dark:hover:bg-zinc-800"
+        v-for="item in menuArr"
+        :key="item.id"
+      >
+        <m-svg-icon
+          :name="item.icon"
+          class="w-1.5 h-1.5 mr-1"
+          fillClass="fill-zinc-900 dark:fill-zinc-300"
+        ></m-svg-icon>
+        <span class="text-zinc-800 dark:text-zinc-300 text-sm">{{
+          item.title
+        }}</span>
+      </div>
+    </div>
+  </m-popover>
 </template>
 
-<script setup></script>
+<script setup>
+const menuArr = [
+  {
+    id: 0,
+    title: '个人资料',
+    icon: 'profile',
+    path: '/'
+  },
+  {
+    id: 1,
+    title: '升级 VIP',
+    icon: 'vip-profile',
+    path: '/'
+  },
+  {
+    id: 2,
+    title: '退出登录',
+    icon: 'logout',
+    path: ''
+  }
+]
+</script>
 
 <style lang="scss" scoped></style>
