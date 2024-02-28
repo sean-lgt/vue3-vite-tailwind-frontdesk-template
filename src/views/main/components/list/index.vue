da3a9480ff9908f0dae9eeaee7ec8723aad759e8
林桂涛 <852951042@qq.com>
2024-02-28 09:42:19 +0800

    feat: 构建基础列表展示

--- src/views/main/components/list/index.vue
+++ src/views/main/components/list/index.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>list</div>
+  <div>
+    <list-item
+      v-for="(item, index) in pexelsList"
+      :key="item.id"
+      :data="item"
+    ></list-item>
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import listItem from './list-item/index.vue'
 import { getPexlesListApi } from '@/api/pexels.js'
 
 let listQuery = {
   page: 1,
   size: 20
 }
+const pexelsList = ref([])
 const getPexlesData = async () => {
   const res = await getPexlesListApi(listQuery)
   console.log('🚀【pexles 列表数据】', res)
+  pexelsList.value = res.list || []
 }
 getPexlesData()
 </script>
