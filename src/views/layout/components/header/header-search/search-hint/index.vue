b9833df2f38a67572671fdbd537fe63d50fb0536
sean-lgt <852951042@qq.com>
2024-03-02 12:08:51 +0800

    perf: 搜索功能增加防抖效果 & 处理关键字高亮显示

--- src/views/layout/components/header/header-search/search-hint/index.vue
+++ src/views/layout/components/header/header-search/search-hint/index.vue
@@ -5,14 +5,14 @@
       :key="index"
       class="py-1 pl-1 text-base font-bold text-zinc-500 rounded cursor-pointer duration-300 hover:bg-zinc-200 dark:hover:bg-zinc-900"
       @click="onItemClick(item)"
-    >
-      {{ item }}
-    </div>
+      v-html="highlightText(item)"
+    ></div>
   </div>
 </template>
 
 <script setup>
 import { ref, watch } from 'vue'
+import { watchDebounced } from '@vueuse/core'
 import { getPexlesSearchHintApi } from '@/api/pexels.js'
 
 const props = defineProps({
@@ -34,16 +34,22 @@ const getHintData = async () => {
   hintData.value = resData.result || []
 }
 
-// 监听
-watch(
-  () => props.searchText,
-  () => {
-    getHintData()
-  },
-  {
-    immediate: true
-  }
-)
+// 监听 增加防抖
+watchDebounced(() => props.searchText, getHintData, {
+  immediate: true,
+  // 每次事件触发时，延迟的时间
+  debounce: 500
+})
+
+// 处理关键字高亮
+const highlightText = (text) => {
+  // 生成高亮标签
+  const highlightStr = `<span class="text-zinc-900 dark:text-zinc-200">${props.searchText}</span>`
+  // 构建正则表达式，从《显示文本中》找出与《用户输入文本相同的内容》，使用《高亮标签》进行替换
+  const reg = new RegExp(props.searchText, 'gi')
+  // 替换
+  return text.replace(reg, highlightStr)
+}
 
 // 点击搜索项
 const onItemClick = (item) => {
