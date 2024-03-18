684d431315630b8d01cb5def6f166868521cfa33
林桂涛 <852951042@qq.com>
2024-03-18 15:33:29 +0800

    feat: 构建移动端下的 tabbar 组件

--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -6,12 +6,61 @@
     <div class="max-w-screen-xl mx-auto relative m-1 xl:mt-4">
       <list />
     </div>
+    <!-- 移动端 tabbar -->
+    <m-trigger-menu
+      v-if="isMobileTerminal"
+      class="fixed bottom-6 m-auto left-0 right-0 w-[220px]"
+    >
+      <m-trigger-menu-item
+        icon="home"
+        iconClass="fill-zinc-900 dark:fill-zinc-200"
+      >
+        首页
+      </m-trigger-menu-item>
+      <m-trigger-menu-item
+        v-if="$store.getters.token"
+        icon="vip"
+        iconClass="fill-zinc-400 dark:fill-zinc-500"
+        textClass="text-zinc-400 dark:text-zinc-500"
+        @click="onVipClick"
+      >
+        VIP
+      </m-trigger-menu-item>
+      <m-trigger-menu-item
+        icon="profile"
+        iconClass="fill-zinc-400 dark:fill-zinc-500"
+        textClass="text-zinc-400 dark:text-zinc-500"
+        @click="onMyClick"
+      >
+        {{ $store.getters.token ? '我的' : '登录' }}
+      </m-trigger-menu-item>
+    </m-trigger-menu>
   </div>
 </template>
 
 <script setup>
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 import navigation from './components/navigation/index.vue'
 import list from './components/list/index.vue'
+import { isMobileTerminal } from '@/utils/flexible.js'
+
+const store = useStore()
+const router = useRouter()
+
+// 跳转vip
+const onVipClick = () => {}
+
+// 跳转我的
+const onMyClick = () => {
+  // 配置跳转方式
+  // store.commit('app/changeRouterType', 'push')
+  if (store.getters.token) {
+    router.push('/profile')
+  } else {
+    router.push('/login')
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
